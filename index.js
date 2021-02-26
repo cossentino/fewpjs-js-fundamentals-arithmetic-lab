e77ad901baf99f4119a4d0f54d84a7c79836dc70
Ian Cossentino <icossentino@gmail.com>
2021-02-26 15:35:25 -0500

    commit tree d8e36273a050d38d166bc6beb1eefd515291895e

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+
+// Problem 1
+var num1 = 31
+var num2 = 2
+
+var multiply = num1 * num2
+
+
+// Problem 2
+var random = Math.abs(Math.floor(Math.random() * 10 + 1))
+
+
+// Problem 3
+var num3 = 84
+var num4 = 20
+
+var mod = num3 % num4
+
+
+// Problem 4
+
+var max = Math.max(1,2,3,4,20)
